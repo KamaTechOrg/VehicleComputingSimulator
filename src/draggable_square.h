951d115ca6e1bd69b949696a02d82b5bbd090211
Dvora Novogrotzki <d3254766@gmail.com>
2024-08-14 23:29:56 +0300

    GUI: Dynamic frames with information from log file

--- src/draggable_square.h
+++ src/draggable_square.h
@@ -1,9 +1,10 @@
 #ifndef __DRAGGABLE_SQUARE_H__
 #define __DRAGGABLE_SQUARE_H__
 
-#include <QWidget>
 #include <QLabel>
 #include <QMouseEvent>
+#include <QWidget>
+#include <QPushButton>
 #include "process.h"
 
 class DraggableSquare : public QWidget
@@ -11,25 +12,36 @@ class DraggableSquare : public QWidget
     Q_OBJECT
 
 public:
-    explicit DraggableSquare(QWidget *parent = nullptr,const QString &color = "background-color: green;",int width=100,int height=100);
-    DraggableSquare(const DraggableSquare &other); // Copy constructor
-    DraggableSquare &operator=(const DraggableSquare &other); // Copy assignment operator
-    
+    explicit DraggableSquare(QWidget *parent = nullptr,
+                             const QString &color = "background-color: green;",
+                             int width = 100, int height = 100);
+    DraggableSquare(const DraggableSquare &other);  // Copy constructor
+    DraggableSquare &operator=(
+        const DraggableSquare &other);  // Copy assignment operator
+
     void setProcess(const Process &process);
     const Process getProcess() const;
     const QPoint getDragStartPosition() const;
+    void setDragStartPosition(QPoint dragStartPosition);
     void setSquareColor(const QString &color);
     void print() const;
-    
+
 protected:
     void mousePressEvent(QMouseEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
 
 private:
     QPoint dragStartPosition;
     QPoint initialPosition;
     QLabel *label;
     Process process;
+    int id;
+    bool dragging;
+
+private slots:
+    void editSquare(int id);
+    void deleteSquare(int id);
 };
 
-#endif // __DRAGGABLE_SQUARE_H__
+#endif  // __DRAGGABLE_SQUARE_H__
--- src/process_dialog.h
+++ src/process_dialog.h
@@ -1,15 +1,14 @@
 #ifndef PROCESSDIALOG_H
 #define PROCESSDIALOG_H
 
+#include <QComboBox>
 #include <QDialog>
+#include <QFormLayout>
 #include <QLineEdit>
 #include <QPushButton>
 #include <QVBoxLayout>
-#include <QFormLayout>
-#include <QComboBox>
 
-class ProcessDialog : public QDialog
-{
+class ProcessDialog : public QDialog {
     Q_OBJECT
 
 public:
@@ -20,6 +19,10 @@ public:
     QString getCMakeProject() const;
     QString getQEMUPlatform() const;
     bool isValid() const;
+    void setId(int id);
+    void setName(const QString &name);
+    void setCMakeProject(const QString &cmakeProject);
+    void setQEMUPlatform(const QString &qemuPlatform);
 
 private slots:
     void validateAndAccept();
@@ -28,8 +31,7 @@ private:
     QLineEdit *idEdit;
     QLineEdit *nameEdit;
     QLineEdit *cmakeProjectEdit;
-    // QLineEdit *qemuPlatformEdit;
     QComboBox *qemuPlatformCombo;
 };
 
-#endif // PROCESSDIALOG_H
+#endif  // PROCESSDIALOG_H
