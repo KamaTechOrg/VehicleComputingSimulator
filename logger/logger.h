bcf2325f2868c1eb15e5740d4fd750593fc7ff86
ayala-freind <0527688937a@gmail.com>
2024-08-15 11:56:27 +0300

    logger with basic tests

---
+++ logger/logger.h
@@ -0,0 +1,41 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <string>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <mutex>
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL logger::LogLevel::INFO
+#endif
+
+class logger {
+public:
+    enum class LogLevel {
+        ERROR,
+        INFO,
+        DEBUG,
+    };
+    logger(){}
+    logger(std::string componentName);
+    void logMessage(LogLevel level, const std::string &message);
+    static void logMessage(LogLevel level, std::string src, std::string dst, const std::string &message);
+    static void initializeLogFile();
+    static std::string getLogFileName();
+
+private:
+    static std::string logLevelToString(LogLevel level);
+    static bool shouldLog(LogLevel level);
+    static std::string getElapsedTime();
+    static std::string componentName;
+    static std::string logFileName;
+    static bool isInitialized;
+    static std::mutex logMutex;
+    static std::chrono::system_clock::time_point initTime;
+};
+
+#endif // LOGGER_H
