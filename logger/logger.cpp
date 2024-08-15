ad12aff60528a02bc51576d4842d71e9a9e7769a
ayala-freind <0527688937a@gmail.com>
2024-08-15 11:56:27 +0300

    logger with basic tests

--- logger/logger.cpp
+++ logger/logger.cpp
@@ -1,26 +1,31 @@
 #include "logger.h"
 
 std::string logger::logFileName;
+bool logger::isInitialized = false;
 std::mutex logger::logMutex;
-std::chrono::system_clock::time_point logger::initTime=std::chrono::system_clock::now();
-std::string logger::componentName = "out";
+std::chrono::system_clock::time_point logger::initTime;
 
 logger::logger(std::string componentName)
 {
-    logger::componentName=componentName;
+    componentName=componentName;
 }
+
 void logger::initializeLogFile()
 {
+    std::lock_guard<std::mutex> guard(logMutex);
+
     if (isInitialized) return;
+
+    initTime = std::chrono::system_clock::now();
 
     auto time = std::chrono::system_clock::to_time_t(initTime);
     std::tm tm = *std::localtime(&time);
 
     std::ostringstream oss;
-    oss << "" << std::put_time(&tm, "%Y_%m_%d_%H_%M_%S") << "_" << componentName << ".log";
+    oss << "../../" << std::put_time(&tm, "%Y_%m_%d_%H_%M_%S") << "_" << componentName << ".log";
     logFileName = oss.str();
-    
-    std::ofstream sharedFile(sharedLogFileName, std::ios::out | std::ios::trunc);
+
+    std::ofstream sharedFile("shared_log_file_name.txt", std::ios::out | std::ios::trunc);
     if (sharedFile) {
         sharedFile << logFileName;
     } else {
@@ -33,24 +38,19 @@ void logger::initializeLogFile()
 std::string logger::getLogFileName()
 {
     if (!isInitialized) {
+        std::lock_guard<std::mutex> guard(logMutex);
         if (!isInitialized) {
-            std::ifstream sharedFile(sharedLogFileName);
+            std::ifstream sharedFile("shared_log_file_name.txt");
             if (sharedFile) {
                 std::getline(sharedFile, logFileName);
             }
             if (logFileName.empty()) {
-                std::cout<<"getLogFileName"<<std::endl;
                 initializeLogFile();
             }
         }
     }
 
     return logFileName;
-}
-
-void logger::cleanUp()
-{
-    std::remove(sharedLogFileName.c_str());
 }
 
 std::string logger::logLevelToString(LogLevel level)
@@ -86,15 +86,16 @@ std::string logger::getElapsedTime()
 
 void logger::logMessage(LogLevel level, std::string src, std::string dst, const std::string &message)
 {
-    std::cout<<"logMessage"<<std::endl;
     if (!shouldLog(level)) return;
 
     std::lock_guard<std::mutex> guard(logMutex);
+
     std::ofstream logFile(getLogFileName(), std::ios_base::app);
     if (!logFile) {
         std::cerr << logLevelToString(LogLevel::ERROR) << "Failed to open log file" << std::endl;
         return;
     }
+
     logFile << "[" << getElapsedTime() << "] "
             << logLevelToString(level) << " "
             << "SRC " << src << " "
--- logger/logger.h
+++ logger/logger.h
@@ -9,18 +9,8 @@
 #include <iostream>
 #include <mutex>
 
-#ifndef LOG_LVL
-    #define LOG_LVL 1
-#endif
-
-#if LOG_LVL == 0
-    #define LOG_LEVEL logger::LogLevel::ERROR
-#elif LOG_LVL == 1
-    #define LOG_LEVEL logger::LogLevel::INFO
-#elif LOG_LVL == 2
-    #define LOG_LEVEL logger::LogLevel::DEBUG
-#else
-    #define LOG_LEVEL logger::LogLevel::INFO
+#ifndef LOG_LEVEL
+#define LOG_LEVEL logger::LogLevel::INFO
 #endif
 
 class logger {
@@ -33,18 +23,17 @@ public:
     logger(){}
     logger(std::string componentName);
     void logMessage(LogLevel level, const std::string &message);
-    void logMessage(LogLevel level, std::string src, std::string dst, const std::string &message);
-    void initializeLogFile();
-    std::string getLogFileName();
-    std::string sharedLogFileName = "shared_log_file_name.txt";
-    void cleanUp();
+    static void logMessage(LogLevel level, std::string src, std::string dst, const std::string &message);
+    static void initializeLogFile();
+    static std::string getLogFileName();
+
 private:
     static std::string logLevelToString(LogLevel level);
     static bool shouldLog(LogLevel level);
     static std::string getElapsedTime();
     static std::string componentName;
     static std::string logFileName;
-    bool isInitialized = false;
+    static bool isInitialized;
     static std::mutex logMutex;
     static std::chrono::system_clock::time_point initTime;
 };
