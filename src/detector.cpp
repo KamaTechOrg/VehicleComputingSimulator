9ce1a0e55c6c0ad3e8d65b53f46fc2111da54a1b
MiriErlbaum <miri26435@gmail.com>
2024-09-05 15:00:24 +0300

    Img_processing: optimize detection

--- src/detector.cpp
+++ src/detector.cpp
@@ -5,6 +5,20 @@ using namespace dnn;
 
 void Detector::detect(const shared_ptr<Mat>& frame)
 {
+    //intialize variables
+    output.clear();
+    this->prevFrame = this->currentFrame;
+    this->currentFrame = frame;
+    if (!prevFrame) {
+        detectObjects(currentFrame, Point(0, 0));
+    }
+    else {
+        detectChanges();
+    }
+}
+
+void Detector::detectObjects(const shared_ptr<Mat>& frame, const Point& position)
+{
     /*
     Performs object detection on a video media using the YOLOv5 model
     Converts the input images to a blob suitable for model input
@@ -13,9 +27,8 @@ void Detector::detect(const shared_ptr<Mat>& frame)
     Applies confidence and threshold filtering and performs
     non-maximum suppression to eliminate redundant detections
     */
-    this->frame = frame;
     Mat blob;
-    auto inputImage = formatYolov5();
+    auto inputImage = formatYolov5(frame);
     blobFromImage(inputImage, blob, 1. / 255., Size(INPUT_WIDTH, INPUT_HEIGHT),
                   Scalar(), true, false);
     net.setInput(blob);
@@ -71,12 +84,92 @@ void Detector::detect(const shared_ptr<Mat>& frame)
     }
 }
 
+void Detector::detectChanges()
+{
+    const vector<Rect> changedAreas = findDifference();
+    cout << "changedAreas:" << changedAreas.size() << endl;
+    //TODO : delete all rects that contained in another rect
+    for (Rect oneChange : changedAreas) {
+        int x = oneChange.x;
+        int y = oneChange.y;
+        Point position(x, y);
+        Mat view(*currentFrame, oneChange);
+        detectObjects(make_shared<Mat>(view), position);
+    }
+}
+
+vector<Rect> Detector::findDifference()
+{
+    vector<Rect> differencesRects;
+    Mat prevGray, currentGray;
+    cvtColor(*prevFrame, prevGray, COLOR_BGR2GRAY);
+    cvtColor(*currentFrame, currentGray, COLOR_BGR2GRAY);
+    // Find the difference between the two images
+    Mat diff;
+    absdiff(prevGray, currentGray, diff);
+    // Apply threshold
+    Mat thresh;
+    threshold(diff, thresh, 0, 255, THRESH_BINARY | THRESH_OTSU);
+    // Dilation
+    Mat kernel = Mat::ones(5, 5, CV_8U);
+    Mat dil;
+    dilate(thresh, dil, kernel, Point(-1, -1), 2);
+    // Calculate contours
+    std::vector<std::vector<Point>> contours;
+    findContours(dil, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+    for (const auto &contour : contours) {
+        // Calculate bounding box around contour
+        Rect boundingBox = boundingRect(contour);
+        differencesRects.push_back(boundingBox);
+    }
+    cout << "num of difference:" << differencesRects.size() << endl;
+    vector<Rect> unionRects = unionOverlappingRectangels(differencesRects);
+    cout << "after union:" << unionRects.size() << endl;
+    return unionRects;
+}
+
+vector<Rect> Detector::unionOverlappingRectangels(vector<Rect> allChanges)
+{
+    vector<bool> isValid(allChanges.size(), true);
+    for (int i = 0; i < allChanges.size(); i++) {
+        if (isValid[i]) {
+            for (int j = 0; j < allChanges.size(); j++) {
+                if (isValid[j]) {
+                    if (i != j && (allChanges[i] & allChanges[j]).area() > 0) {
+                        if (i > j) {
+                            allChanges[i] |= allChanges[j];
+                            isValid[j] = false;
+                        }
+                        else {
+                            allChanges[j] |= allChanges[i];
+                            isValid[i] = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    for (int i = 0; i < allChanges.size(); i++) {
+        if (!isValid[i]) {
+            isValid.erase(isValid.begin() + i);
+            allChanges.erase(allChanges.begin() + i);
+            i--;
+        }
+    }
+    for (Rect r : allChanges) {
+        rectangle(*currentFrame, r, Scalar(0, 0, 255), 2);
+    }
+    imshow("win", *currentFrame);
+    waitKey();
+    return allChanges;
+}
+
 vector<DetectionObject> Detector::getOutput() const
 {
     return output;
 }
 
-Mat Detector::formatYolov5()
+Mat Detector::formatYolov5(const shared_ptr<Mat>& frame)
 {
     int col = frame->cols;
     int row = frame->rows;
