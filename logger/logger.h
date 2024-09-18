7801aa1724ed1357006532d6a7408cfca931180e
Chaya Roten <c0548560049@gmail.com>
2024-09-18 14:35:12 +0300

    GUI: Fix logger files to comply with coding conventions

--- logger/logger.h
+++ logger/logger.h
@@ -15,30 +15,30 @@
 
 class logger {
 public:
-  enum class LogLevel {
-    ERROR,
-    INFO,
-    DEBUG,
-  };
-  logger() {}
-  logger(std::string componentName);
-  void logMessage(LogLevel level, const std::string &message);
-  void logMessage(LogLevel level, std::string src, std::string dst,
-                  const std::string &message);
-  void initializeLogFile();
-  std::string getLogFileName();
-  std::string sharedLogFileName = "shared_log_file_name.txt";
-  void cleanUp();
+    enum class LogLevel {
+        ERROR,
+        INFO,
+        DEBUG,
+    };
+    logger() {}
+    logger(std::string componentName);
+    void logMessage(LogLevel level, const std::string &message);
+    void logMessage(LogLevel level, std::string src, std::string dst,
+                    const std::string &message);
+    void initializeLogFile();
+    std::string getLogFileName();
+    std::string sharedLogFileName = "shared_log_file_name.txt";
+    void cleanUp();
 
 private:
-  static std::string logLevelToString(LogLevel level);
-  static bool shouldLog(LogLevel level);
-  static std::string getElapsedTime();
-  static std::string componentName;
-  static std::string logFileName;
-  bool isInitialized = false;
-  static std::mutex logMutex;
-  static std::chrono::system_clock::time_point initTime;
+    static std::string logLevelToString(LogLevel level);
+    static bool shouldLog(LogLevel level);
+    static std::string getElapsedTime();
+    static std::string componentName;
+    static std::string logFileName;
+    bool isInitialized = false;
+    static std::mutex logMutex;
+    static std::chrono::system_clock::time_point initTime;
 };
 
-#endif // LOGGER_H
+#endif  // LOGGER_H
