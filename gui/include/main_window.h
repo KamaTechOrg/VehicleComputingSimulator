ad17ba781251e27beaf34f56d4511bb4f0c2ddb2
Tamar Leibovitz <t027163112@gmail.com>
2024-09-27 02:05:04 +0300

    GUI: handle process crash
    Generate core dump, notify user, stop system if initial process crashes

--- gui/include/main_window.h
+++ gui/include/main_window.h
@@ -44,6 +44,8 @@ class MainWindow : public QMainWindow {
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    std::string getCoreDumpPath(qint64 pid, const std::string &executableName);
+    void setCoreDumpLimit();
     void updateTimer();
     void endProcesses();
     void stopProcess(int deleteId);
@@ -55,7 +57,6 @@ public:
     void enableAllButtons();         // Re-enable all buttons
     void showLoadingIndicator();     // Show loading animation (spinner)
     void hideLoadingIndicator();     // Hide loading animation (spinner)
-  
     QLineEdit *getTimeInput() const
     {
         return timeInput;
@@ -108,7 +109,7 @@ private:
     void rotateImage();     // Function to handle rotation
     void openSecondProject();  // Function that handles launching the second project
     void setDefaultBackgroundImage();
-    void openDialog();
+    void openDialog();   
     QPushButton *openSecondProjectButton;  // Button to open the second project
     QVBoxLayout *toolboxLayout;
     QWidget *workspace;
