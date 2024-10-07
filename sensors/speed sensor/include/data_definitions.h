b289c74d2025ae8c1ec2dae3bfdaded4b8ef2e08
Lea Weiss <leaweiss206@gmail.com>
2024-10-07 15:22:15 +0300

    Control: Add writing to logger in speed sensor
    Co-authored-by: Miri Ben Chaim <mbh05567@gmail.com>
    Co-authored-by: Lea Weiss <leaweiss206@gmail.com>

---
+++ sensors/speed sensor/include/data_definitions.h
@@ -0,0 +1,39 @@
+#ifndef __DATA_DEFINITIONS__
+#define __DATA_DEFINITIONS__
+
+#include <Arduino.h>
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include "../../../communication/src/communication.h"
+#include "../../../logger/logger.h"
+
+// Defines
+#define MAX_NORMAL_VALUE 125
+#define REASONABLE_GAP 20
+#define INVALID_CONTINUITY 6
+#define WAIT_TIME 1000
+#define SRC_ID 8
+#define MSG_SIZE 64
+#define SENDING_TIME 20
+
+
+
+
+// Data
+extern String data;
+
+//members
+extern bool status;
+extern float scaleValue;
+extern Communication *com;
+extern logger *speedLog;
+
+//functions
+void sendMessage(std::string message, int speed);
+void handleMesseage(uint32_t senderId , void *data);
+
+
+
+#endif // __DATA_DEFINITIONS__
--- sensors/speed sensor/src/main.cpp
+++ sensors/speed sensor/src/main.cpp
@@ -1,103 +1,29 @@
-#include <Arduino.h>
-#include <iostream>
-#include <thread>
-#include <chrono>
-
-#include "../../../communication/src/communication.h"
-
-#define MAX_NORMAL_VALUE 125
-#define REASONABLE_GAP 20
-#define INVALID_CONTINUITY 6
-#define WAIT_TIME 1000
-#define SRC_ID 8
-#define MSG_SIZE 64
+#include "data_definitions.h"
 
 using namespace std;
-
-String data =
-    "89,93,95,96,99,102,104,106,105,423,107,108,112,115,116,118,120,121,121,"
-    "119,121,118,119,121,367,125,125,126,127,127,127,127,127,127,128,124,120,"
-    "119,118,116,113,111,109,107,105,103,100,98,96,94,93,92,92,91,92,93,94,190,"
-    "98,99,99,99,9992,94,96,97,0,102,104,104,405,100,98,95,277,89,84,82,80,77,"
-    "73,68,67,67,68,69,67,64,64,64,64,66"
-    "56,46,49,51,51,48,46,55,54,54,43,44,46,42,39,41,46,47,51,50,49,49,48,49,"
-    "47,51,49,50,48,39,52,48,40,46,44,46,45,46,46,48,45,45,44,43,43,169,158,40,"
-    "39,44,43,44,45,45,43,137,42,45,42,44,43,168,88,36,39,39,42,36,42,37,47,"
-    "177,47,48,50,52,48,46,46,45,39,47,49,49,51,49,61,46,48,50,51,55,49,51,49,"
-    "52,49,51,49,53,51,57,56,57,173,60,59,57,55,55,55,52,50,51,50,46,45,46,45,"
-    "45,45,44,52,93,47,51,53,56,58,60,60,54,117,49,59,0,0,0,0,0,0,0,0,0,0,0,0,"
-    "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"
-    "0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"
-    "0,0,0,0,0,0,0,0,0,0,3,2,2,10,10,14,19,27,31,130,34,35,35,106,35,33,41,50,"
-    "52,110,62,70,76,81,84,86,85,86,87,88,89,89,91,92,93,94,94,371,92,93,92,89,"
-    "88,173,85,85,85,258,88,90,92,94,96,97,0,102,104,104,405,100,98,95,277,89,"
-    "84,82,80,77,73,68,67,67,68,69,67,64,64,64,64,66,205,69,70,72,74,76,76,76,"
-    "76,78,238,82,84,87,89,93,95,96,99,102,104,106,105,423,107,108,112,115,116,"
-    "118,120,121,121,119,121,118,119,121,367,125,125,126,127,127,127,127,127,"
-    "127,128,124,120,119,118,116,113,111,109,107,105,103,100,98,96,94,93,92,92,"
-    "91,92,93,94,190,98,99,99,99,99";
 
 int currentIndex, lastCommaIndex, countLoop, countInvalid;
 int average, previousValue;
 bool status;
 float scaleValue;
 Communication *com;
+logger *speedLog;
 
-void sendMessage(string message, int speed)
-{
-    std::vector<uint8_t> buffer;
-    buffer.push_back(static_cast<uint8_t>(status ? 1 : 0));
-
-    int i = 0;
-    for (;i < message.size() &&  i < MSG_SIZE; buffer.push_back(static_cast<uint8_t>(message[i])), i++ )
-        ;
-    for (; i < MSG_SIZE; buffer.push_back(0), i++)
-        ; 
-
-    uint8_t intFieldBytes[4];
-    std::memcpy(intFieldBytes, &speed, sizeof(int32_t));
-   
-    // std::cout << std::dec << std::endl; 
-    // buffer.push_back(intFieldBytes[3]);  // Most significant byte
-    // buffer.push_back(intFieldBytes[2]);
-    // buffer.push_back(intFieldBytes[1]);
-    // buffer.push_back(intFieldBytes[0]);  // Least significant byte   
-
-    int32_t flags = speed;
-    uint8_t flagsBytes[4];
-    std::memcpy(flagsBytes, &flags, sizeof(int32_t));
-    buffer.insert(buffer.end(), flagsBytes, flagsBytes + 4) ;
-
-    com->sendMessage(buffer.data(), buffer.size(), 1, SRC_ID, false);
-}
-
-void handleMesseage(uint32_t senderId , void *data)
-{
-    string  mesagge = static_cast<char *>(data);
-    Serial.println("data recieved");
-    Serial.println(mesagge.c_str());
-
-    if(mesagge == "slow down")
-        scaleValue *= 0.95;
-    else if(mesagge == "speed up")
-        scaleValue *= 1.05;
-
-    free(data);
-}
 
 void setup()
 {
     Serial.begin(115200);
-    Serial.println("speed sonsor");
-    currentIndex = 0;
-    lastCommaIndex = -1;
-    countLoop = 0;
-    average = 0;
-    previousValue = -1;
-    status = true;
+    currentIndex = countLoop = average = countInvalid = 0 ;
+    lastCommaIndex = previousValue = -1;
     scaleValue = 1;
+    status = true;
     com = new Communication(SRC_ID,handleMesseage);
     com->startConnection();
+    speedLog = new logger("speed sensor");
+    speedLog->logMessage(logger::LogLevel::INFO,"speed sensor");
+    //Serial.println("speed sensor");
+
+
 }
 
 void loop()
@@ -111,19 +37,20 @@ void loop()
 
         String numberStr = data.substring(currentIndex, nextCommaIndex);
         int currentValue = numberStr.toInt() * scaleValue;
-
-        Serial.println(currentValue);
+        speedLog->logMessage(logger::LogLevel::DEBUG,"currentValue: " + to_string(currentValue));
+         Serial.println(currentValue);
 
         currentIndex = nextCommaIndex + 1;
 
         if (currentValue > MAX_NORMAL_VALUE) {
             countInvalid++;
+            speedLog->logMessage(logger::LogLevel::DEBUG, "invalid value " + to_string(currentValue) + " " + to_string(countInvalid) +" times" );
+
             if (countInvalid > INVALID_CONTINUITY) {
                 status = false;
-                Serial.println("");
-                Serial.println("send error:");
-                Serial.println(("--- " + std::to_string(currentValue) + " ---").c_str());
-                Serial.println("");
+                Serial.println("\n send error:");
+                Serial.println(("--- " + std::to_string(currentValue) + " --- \n").c_str());
+                speedLog->logMessage(logger::LogLevel::ERROR, "send error:" +to_string(currentValue));
                 countLoop = 0;
                 average = 0;
                 sendMessage("high speed",currentValue);
@@ -137,6 +64,7 @@ void loop()
                 previousValue == -1) {
                 average += currentValue;
                 previousValue = currentValue;
+                speedLog->logMessage(logger::LogLevel::INFO,"unlikely gap, previous value: " + to_string(previousValue) + " current value: " + to_string(currentValue));
             }
             else {
                 average += previousValue;
@@ -146,11 +74,10 @@ void loop()
     else {
         currentIndex = 0;
     }
-    if (countLoop == 20) {
-        Serial.println("");
-        Serial.println("send:");
-        Serial.println(("--- " + std::to_string((average / 20)) + " ---").c_str());
-        Serial.println("");
+    if (countLoop == SENDING_TIME) {
+        // Serial.println("\n send:");
+        // Serial.println(("--- " + std::to_string((average / 20)) + " --- \n").c_str());
+        speedLog->logMessage(logger::LogLevel::INFO,"send: " + to_string(average / 20));
 
         sendMessage("average current speed",(average/20));
 
